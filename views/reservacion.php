269c49cc733eb000af2de1158c47359c05736083
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-10-10 21:31:46 -0500

    Update reservacion.php

--- views/reservacion.php
+++ views/reservacion.php
@@ -11,19 +11,6 @@
     $User = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `users` WHERE user_id = $id_user"));
     ?>
 </head>
-<?php
-if (isset($_POST['cliente'])) {
-	echo "Mostrar informacion del cliente";
-}else{
-	echo "Mostrar buscador selector de cliente";
-}
-if (isset($_POST['habitacion'])){
-	echo "Mostrar informacion de habitacion";
-}else{
-	echo "Seleccionar habitacion y mostrar informacion";
-}
-
-?>
 <body>
 	<div class="container">
 		<div class="row col s12">
@@ -33,54 +20,63 @@ if (isset($_POST['habitacion'])){
 		            <li class="collection-item avatar">
 		              <img src="../img/cliente.png" alt="" class="circle">
 		              <span class="title"><b>DETALLES DEL CLIENTE</b></span><br><br>
+		              <?php
+						if (isset($_POST['cliente'])) {
+							echo "Mostrar informacion del cliente";
+						}else{
+							$NOMBRE = ''; $DIRECCION = ''; $COLONIA = ''; $LOCALIDAD = ''; $CP = '';
+							$EMAIL = ''; $TELEFONO = ''; $LIMPIEZA = ''; $IdCliente = 0;
+						}
+					  ?>
 		              <p class="row col s12"><b>
+		              	<input type="hidden" id="id_cliente" value="<?php echo $IdCliente;?>">
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>NOMBRE: </b>
 		              		<div class="col s12 m8">
-				              <input id="nombreCliente" type="text" class="validate" data-length="100" value="<?php echo 'NOMBRE DEL cliente';?>" required>	
+				              <input id="nombreCliente" type="text" class="validate" data-length="100" value="<?php echo $NOMBRE;?>" required>	
 				            </div>
 		              	</div>
 		              	<div class="col s12" id="clienteBusqueda"><br><br></div>
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>DIRECCION: </b>
 		              		<div class="col s12 m8">
-				              <input id="direccionCliente" type="text" class="validate" data-length="100" value="<?php echo 'DIRECCION DEL cliente';?>" required>	
+				              <input id="direccion" type="text" class="validate" data-length="100" value="<?php echo $DIRECCION;?>" required>	
 				            </div>
 		              	</div>     
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>COLONIA: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'colonia DEL cliente';?>" required>	
+				              <input id="colonia" type="text" class="validate" data-length="20" value="<?php echo $COLONIA;?>" required>	
 				            </div>
 		              	</div>   
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>LOCALIDAD: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'colonia DEL cliente';?>" required>	
+				              <input id="localidad" type="text" class="validate" data-length="20" value="<?php echo $LOCALIDAD;?>" required>	
 				            </div>
 		              	</div>  
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4">CODIGO POSTAL: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'colonia DEL cliente';?>" required>	
+				              <input id="cp" type="text" class="validate" data-length="6" value="<?php echo $CP;?>" required>	
 				            </div>
 		              	</div>  
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>EMAIL: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'colonia DEL cliente';?>" required>	
+				              <input id="email" type="text" value="<?php echo $EMAIL;?>" required>	
 				            </div>
 		              	</div>
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>TELEFONO: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'colonia DEL cliente';?>" required>	
+				              <input id="telefono" type="text" class="validate" data-length="10" value="<?php echo $TELEFONO;?>" required>	
 				            </div>
 		              	</div>
 		              	<div class="col s12">
 		              		<b class="indigo-text col s12 m4"><br>LIMPIEZA: </b>
 		              		<div class="col s12 m8">
-				              <input id="coloniaCliente" type="text" class="validate" data-length="100" value="<?php echo 'LIMPIAR ASI Y ASI';?>" required>	
+				              <input id="limpieza" type="text" class="validate" data-length="100" value="<?php echo $LIMPIEZA;?>" required>	
 				            </div>
 		              	</div>		            		
 		              </b></p>
@@ -96,15 +92,30 @@ if (isset($_POST['habitacion'])){
 		              <span class="title"><b>DETALLES DE HABITACION</b></span><br><br>
 		              <p class="row col s12"><b>
 		              	<div class="col s12"><br>
-		              		<b class="indigo-text col s12 m5"><br>N° HABITACION: </b>
-		              		<div class="col s12 m7">
-					            <select id="piso" name="piso" class="validate">              
+		              		<b class="indigo-text col s12 m5"><br>HABITACION: </b>
+		              		<div class="col s12 m6">
+					            <select id="habitacion" name="habitacion" class="validate">              
 					              <!--OPTION PARA QUE LA SELECCION QUEDE POR DEFECTO-->
-					              <option value="0" select>Nievel / Piso</option>
-					              <option value="Primer">Primer</option>
-					              <option value="Segundo">Segundo</option>
-					              <option value="Tercer">Tercer</option>
-					              <option value="Cuarto">Cuarto</option>
+					              <?php
+			              			if (isset($_POST['habitacion'])){
+										echo '<option value="'.$_POST['habitacion'].'" select>N° '.$_POST['habitacion'].'</option>';
+									}else{
+										echo '<option value="0" select>Habitaciones:</option>';
+									}
+									$consulta = mysqli_query($conn,"SELECT * FROM habitaciones");	
+					                //VERIFICAMOS QUE LA VARIABLE SI CONTENGA INFORMACION
+					                if (mysqli_num_rows($consulta) == 0) {
+					                    echo '<script>M.toast({html:"No se encontraron habitaciones.", classes: "rounded"})</script>';
+					                } else {
+					                    //RECORREMOS UNO A UNO LOS ARTICULOS CON EL WHILE
+					                    while($habitacion = mysqli_fetch_array($consulta)) {
+					                      //Output
+					                      ?>
+					                      <option value="<?php echo $habitacion['id'];?>">N° <?php echo $habitacion['id'].' - '.$habitacion['tipo'];?></option>
+					                      <?php
+					                    }//FIN while
+					                }//FIN else
+					                ?>
 					            </select>
 					        </div>
 		              	</div>
