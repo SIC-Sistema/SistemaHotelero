cc2962c484608795bcb9ac2daa2f005f48476f89
fredy1617 <36872787+fredy1617@users.noreply.github.com>
2022-10-14 09:01:25 -0500

    Update detalles_cliente.php

--- views/detalles_cliente.php
+++ views/detalles_cliente.php
@@ -6,24 +6,24 @@
     <?php
     //INCLUIMOS EL ARCHIVO QUE CONTIENE LA BARRA DE NAVEGACION TAMBIEN TIENE (scripts, conexion, is_logged, modals)
     include('fredyNav.php');
-	$id_user = $_SESSION['user_id'];// ID DEL USUARIO LOGEADO
+		$id_user = $_SESSION['user_id'];// ID DEL USUARIO LOGEADO
     //Obtenemos la informacion del Usuario
     $User = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `users` WHERE user_id = $id_user"));
 
-	//VERIFICAMOS QUE SI NOS ENVIE POR POST EL ID DEL ARTICULO
-	if (isset($_GET['id']) == false OR $User['clientes'] == 0) {
-	  ?>
-	  <script>    
-	    M.toast({html:"Regresando a clientes.", classes: "rounded"});
-	    setTimeout("location.href='clientes.php'", 500);
-	  </script>
-	  <?php
-	}else{
-		$id = $_GET['id'];// POR EL METODO POST RECIBIMOS EL ID DEL HABITACION
-	    //CONSULTA PARA SACAR LA INFORMACION DE LA HABITACION Y ASIGNAMOS EL ARRAY A UNA VARIABLE $habitacion
-	    $habitacion = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `clientes` WHERE id=$id"));
-	}
-	?>
+		//VERIFICAMOS QUE SI NOS ENVIE POR POST EL ID DEL ARTICULO
+		if (isset($_POST['id']) == false OR $User['clientes'] == 0) {
+		  ?>
+		  <script>    
+		    M.toast({html:"Regresando a clientes.", classes: "rounded"});
+		    setTimeout("location.href='clientes.php'", 500);
+		  </script>
+		  <?php
+		}else{
+			$id = $_POST['id'];// POR EL METODO POST RECIBIMOS EL ID DEL HABITACION
+		    //CONSULTA PARA SACAR LA INFORMACION DE LA HABITACION Y ASIGNAMOS EL ARRAY A UNA VARIABLE $habitacion
+		    $habitacion = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM `clientes` WHERE id=$id"));
+		}
+		?>
 	<script>
 	  //FUNCION QUE BORRA HABITACIONES (SE ACTIVA AL INICIAR EL BOTON BORRAR)
       function borrar_habitacion(id){
